cb1213b1b945e36d54e52fdd5c6bb3adc2b28a6d
PABEGA <pbetancurgarcia@gmail.com>
2024-01-07 22:55:34 -0500

    setup

--- setup.R
+++ setup.R
@@ -1,10 +1,9 @@
 library(blogdown)
 # new_site(theme = "hugo-apero/hugo-apero", format = "toml", sample = FALSE,
 #                   empty_dirs = TRUE)
-
+build_site()
 serve_site()
 stop_server()
 
-
-# git merge origin NOMBRE DE LA RAMA: unir las ramas secundarias a la rama principal
 # git checkout NOMBRE DE LA RAMA: para cambiarse de rama
+# git merge origin NOMBRE DE LA RAMA: unir las ramas secundarias a la rama principal
